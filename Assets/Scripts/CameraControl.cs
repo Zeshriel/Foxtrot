3a4b5f3edfeb2a4c924bd77ca1734051eaf86836
Matthew <mmgligore@gmail.com>
2024-11-09 14:22:26 -0500

    Created Wall Generation
    Wall generation using multiple clones for layering.

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Transform target; // Player
+    public Transform bg1;    // Left wall background 1
+    public Transform bg2;    // Left wall background 2
+    public Transform bg3;    // Right wall background 1
+    public Transform bg4;    // Right wall background 2
+    private float size;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        size = bg1.GetComponent<BoxCollider2D>().size.y;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 targetPos = new Vector3(transform.position.x, target.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPos, 0.2f);
+
+        // Handle left wall backgrounds
+        if (transform.position.y >= bg2.position.y)
+        {
+            bg1.position = new Vector3(bg1.position.x, bg2.position.y + size, bg1.position.z);
+            SwitchBg(ref bg1, ref bg2);
+        }
+        if (transform.position.y < bg1.position.y)
+        {
+            bg2.position = new Vector3(bg2.position.x, bg1.position.y - size, bg2.position.z);
+            SwitchBg(ref bg1, ref bg2);
+        }
+
+        // Handle right wall backgrounds
+        if (transform.position.y >= bg4.position.y)
+        {
+            bg3.position = new Vector3(bg3.position.x, bg4.position.y + size, bg3.position.z);
+            SwitchBg(ref bg3, ref bg4);
+        }
+        if (transform.position.y < bg3.position.y)
+        {
+            bg4.position = new Vector3(bg4.position.x, bg3.position.y - size, bg4.position.z);
+            SwitchBg(ref bg3, ref bg4);
+        }
+    }
+
+    private void SwitchBg(ref Transform bgA, ref Transform bgB)
+    {
+        Transform temp = bgA;
+        bgA = bgB;
+        bgB = temp;
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed;
-    Rigidbody2D myBod;
-    
+    public float initialSpeed = 2f;
+    public float acceleration = 0.01f; // Slow acceleration over time
+    private float currentSpeed;
+    private Rigidbody2D myBod;
 
     // Start is called before the first frame update
     void Start()
     {
         myBod = GetComponent<Rigidbody2D>();
+        currentSpeed = initialSpeed; // Set the initial speed
     }
 
     // Update is called once per frame
     void Update()
     {
-        myBod.velocity += Vector2.up * speed * Time.deltaTime;
+        currentSpeed += acceleration * Time.deltaTime; // Gradually increase speed
+        myBod.velocity = new Vector2(0, currentSpeed); // Apply the current speed as the y-velocity
     }
 }
